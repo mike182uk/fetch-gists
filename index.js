095cda758ee3dd96f40e22e40b343453cf2b5b38
Michael Barrett <mike182uk@gmail.com>
2020-11-19 13:26:30 +0000

    refactor internals

--- index.js
+++ index.js
@@ -18,73 +18,46 @@ async function fetchGists (accessToken) {
     throw new Error('You must supply an access token to retrieve your gists')
   }
 
-  return fetchAllGists([], 1, accessToken)
-}
+  let gists = []
+  let page = 1
+  let hasMore = true
 
-/**
- * Recursively fetch all the gists for an account
- *
- * @param   {Object[]} gists
- * @param   {number} page
- * @param   {string} accessToken
- * @returns {Promise.<Object[]>}
- */
+  while (hasMore) {
+    try {
+      const { body, headers } = await request({
+        url: 'https://api.github.com/gists',
+        headers: {
+          'User-Agent': 'fetch-gists',
+          accept: 'application/vnd.github.v3+json'
+        },
+        qs: {
+          page,
+          per_page: 100,
+          access_token: accessToken
+        },
+        json: true,
+        resolveWithFullResponse: true
+      })
 
-async function fetchAllGists (gists, page, accessToken) {
-  const { gists: retrievedGists, moreToGet } = await fetchPageOfGists(page, accessToken)
+      gists = gists.concat(body)
 
-  gists = gists.concat(retrievedGists)
+      hasMore = headers.link
+        ? headers.link.includes('rel="next"')
+        : false
 
-  if (moreToGet) {
-    page++
+      page++
+    } catch (err) {
+      if (err.statusCode && [401, 403].includes(err.statusCode)) {
+        throw new Error(`${err.error.message}. You can view the documentation at ${err.error.documentation_url}`)
+      }
 
-    return fetchAllGists(gists, page, accessToken)
+      if (err.statusCode && err.statusCode !== 200) {
+        throw new Error(`Expected 200 response code but got ${err.statusCode}`)
+      }
+
+      throw err
+    }
   }
 
   return gists
-}
-
-/**
- * Fetch a page of gists
- *
- * @param   {number} page
- * @param   {string} accessToken
- * @returns {Promise.<Object[]>}
- */
-
-async function fetchPageOfGists (page, accessToken) {
-  const requestOpts = {
-    url: 'https://api.github.com/gists',
-    headers: {
-      'User-Agent': 'fetch-gists',
-      accept: 'application/vnd.github.v3+json'
-    },
-    qs: {
-      page: page,
-      per_page: 100,
-      access_token: accessToken
-    },
-    json: true,
-    resolveWithFullResponse: true
-  }
-
-  try {
-    const { body, headers } = await request(requestOpts)
-
-    const moreToGet = (headers.link)
-      ? headers.link.includes('rel="next"')
-      : false
-
-    return { gists: body, moreToGet }
-  } catch (err) {
-    if (err.statusCode && (err.statusCode === 401 || err.statusCode === 403)) {
-      throw new Error(`${err.error.message}. You can view the documentation at ${err.error.documentation_url}`)
-    }
-
-    if (err.statusCode && err.statusCode !== 200) {
-      throw new Error(`Expected 200 response code but got ${err.statusCode}`)
-    }
-
-    throw err
-  }
 }
